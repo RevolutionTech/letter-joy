c4b2277d9b3d4846cfb896379b212bc2e0b1c4f1
Lucas Connors <lucas.revolutiontech@gmail.com>
2021-04-25 23:20:53 -0700

    feat: add sidebar where players can perform moves

--- src/Board.tsx
+++ src/Board.tsx
@@ -1,12 +1,36 @@
 import { BoardProps } from "boardgame.io/react";
+import { styled } from "@material-ui/core";
 
+import { SIDEBAR_WIDTH, SIDEBAR_PADDING, ActionSidebar } from "./ActionSidebar";
 import { G } from "./game";
 import { PlayerDisplay } from "./PlayerDisplay";
+import theme from "./theme";
+
+const GameTable = styled("div")({
+  display: "flex",
+  backgroundColor: theme.silver,
+});
+
+const PlayerRows = styled("div")({ display: "flex", flexDirection: "column" });
+
+// The SidebarPlaceholder allows the horizontal scrollbar to appear
+// when the sidebar is covering some of the main content
+const SidebarPlaceholder = styled("div")({
+  minWidth: `calc(${SIDEBAR_WIDTH} + ${SIDEBAR_PADDING} * 2)`,
+});
 
 export const LetterJoyBoard = (props: BoardProps) => {
   const g: G = props.G;
   const playerDisplays = Object.values(g.players).map((playerState) => (
     <PlayerDisplay {...playerState} />
   ));
-  return <>{playerDisplays}</>;
+  return (
+    <>
+      <GameTable>
+        <PlayerRows>{playerDisplays}</PlayerRows>
+        <SidebarPlaceholder />
+      </GameTable>
+      <ActionSidebar />
+    </>
+  );
 };
--- src/theme.ts
+++ src/theme.ts
@@ -1,3 +1,9 @@
-const theme = { white: "#FFFFFF", black: "#000000" };
+const theme = {
+  fontFamily: '"Quicksand", "Helvetica Neue", sans-serif',
+
+  white: "#FFFFFF",
+  silver: "#A1A1A1",
+  black: "#000000",
+};
 
 export default theme;
