b447ba81bb114c324d7ff9a3b8fc7679af2c2cde
Lucas Connors <lucas.revolutiontech@gmail.com>
2021-10-05 23:31:11 -0700

    present: add margin between previous clues and preceeding content

--- src/client/phases/chooseClue/sidebar/VotingContent.tsx
+++ src/client/phases/chooseClue/sidebar/VotingContent.tsx
@@ -80,7 +80,7 @@ export const VotingContent = (props: Props) => {
             ]
       }
     >
-      <FormControl component="fieldset">
+      <FormControl component="fieldset" style={{ marginBottom: "16px" }}>
         <RadioGroup
           aria-label="Vote on proposed clues"
           name="proposed-clues"
--- src/client/phases/rearrangeLetters/StarRating.tsx
+++ src/client/phases/rearrangeLetters/StarRating.tsx
@@ -1,8 +1,11 @@
 import _ from "lodash";
+import { styled } from "@material-ui/core";
 
 import { MAX_NUM_STARS, totalStars } from "../../../game/scoring";
 import starFilled from "../../assets/starFilled.svg";
 import starUnfilled from "../../assets/starUnfilled.svg";
+
+const RatingContainer = styled("div")({ marginBottom: "16px" });
 
 interface Props {
   score: number;
@@ -13,13 +16,13 @@ export const StarRating = (props: Props) => {
   const numStarsText = `${numStars}/${MAX_NUM_STARS} stars`;
 
   return (
-    <>
+    <RatingContainer>
       {_.range(numStars).map((_) => (
         <img src={starFilled} alt={numStarsText} />
       ))}
       {_.range(MAX_NUM_STARS - numStars).map((_) => (
         <img src={starUnfilled} alt={numStarsText} />
       ))}
-    </>
+    </RatingContainer>
   );
 };
