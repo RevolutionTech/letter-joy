3ade2da81bb38750d183bd3ea004ab1b5d9180ff
Lucas Connors <lucas.revolutiontech@gmail.com>
2022-07-07 20:39:54 -0700

    observe: add FullStory to client

--- src/index.tsx
+++ src/index.tsx
@@ -2,20 +2,23 @@ import * as React from "react";
 import * as ReactDOM from "react-dom";
 import * as Sentry from "@sentry/react";
 import { BrowserTracing } from "@sentry/tracing";
+import * as FullStory from "@fullstory/browser";
 
 import App from "./client/App";
 import "./index.css";
 
 const SENTRY_DSN =
   "https://69dacec9afd244ec8178e28569fa8e97@o1118184.ingest.sentry.io/6556529";
+const FULLSTORY_ORG_ID = "o-1BSGWC-na1";
 
-// Init Sentry
+// Init Sentry and Fullstory
 if (process.env.NODE_ENV === "production") {
   Sentry.init({
     dsn: SENTRY_DSN,
     integrations: [new BrowserTracing()],
     tracesSampleRate: 1.0,
   });
+  FullStory.init({ orgId: FULLSTORY_ORG_ID });
 }
 
 const mountNode = document.getElementById("app");
