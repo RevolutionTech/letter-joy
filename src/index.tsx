d67a82cc06d930aa88f164cc726b2e7ddfb39fc3
Lucas Connors <lucas.revolutiontech@gmail.com>
2022-07-07 19:15:53 -0700

    observe: add Sentry to client

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,22 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
+import * as Sentry from "@sentry/react";
+import { BrowserTracing } from "@sentry/tracing";
 
 import App from "./client/App";
 import "./index.css";
+
+const SENTRY_DSN =
+  "https://69dacec9afd244ec8178e28569fa8e97@o1118184.ingest.sentry.io/6556529";
+
+// Init Sentry
+if (process.env.NODE_ENV === "production") {
+  Sentry.init({
+    dsn: SENTRY_DSN,
+    integrations: [new BrowserTracing()],
+    tracesSampleRate: 1.0,
+  });
+}
 
 const mountNode = document.getElementById("app");
 ReactDOM.render(<App />, mountNode);
